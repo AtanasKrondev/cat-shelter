95abcd378cfd45bb04e13c445eb95ef39d37ff98
Atanas Krondev <krondev@gmail.com>
2019-09-21 15:11:26 +0300

    Edit form

--- handlers/cats.js
+++ handlers/cats.js
@@ -68,7 +68,7 @@ module.exports = (req, res) => {
                 };
 
                 const allCats = JSON.parse(data);
-                allCats.push({ id: cats.length + 1, ...fields, image: files.upload.name });
+                allCats.push({ id: (cats.length + 1).toString(), ...fields, image: files.upload.name });
                 const json = JSON.stringify(allCats);
 
                 fs.writeFile('./data/cats.json', json, (err) => {
@@ -104,8 +104,47 @@ module.exports = (req, res) => {
 
         res.writeHead(301, { 'location': '/' });
         res.end();
-    }
-    else {
+    } else if (pathname.includes('cats-edit') && req.method === 'GET') {
+        const filePath = path.normalize(
+            path.join(__dirname, '../views/editCat.html')
+        );
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                console.log(err);
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.write('404 File Not Found');
+                res.end();
+                return;
+            }
+            const id = pathname.split('/').pop();
+            const cat = cats.find((cat) => cat.id === id);
+            console.log(cat)
+            let editForm = `<h2>Edit Cat</h2>
+            <label for="name">Name</label>
+            <input type="text" id="name" value="${cat.name}">
+            <label for="description">Description</label>
+            <textarea
+                id="description">${cat.description}</textarea>
+            <label for="image">Image</label>
+            <input type="file" id="image">
+            <label for="group">Breed</label>
+            <select id="group">
+                {{catBreeds}}
+            </select>
+            <button>Edit Cat</button>`
+            const placeholder = breeds.map(breed => `<option value="${breed}">${breed}</option>`);
+            editForm = editForm.replace('{{catBreeds}}', placeholder);
+
+            const modifiedData = data.toString().replace('{{edit}}', editForm);
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(modifiedData);
+            res.end();
+        });
+        // } else if (pathname.includes('/cats-find-new-home') && req.method === 'GET') {
+        // } else if (pathname.includes('/cats-edit') && req.method === 'POST') {
+        // } else if (pathname.includes('/cats-find-new-home') && req.method === 'POST') {
+    } else {
         return true;
     };
 };
