794ae75074b52fe8d17f9c7c86efb0887d0d86e5
Atanas Krondev <krondev@gmail.com>
2019-09-24 12:56:25 +0300

    Search functionality

--- handlers/home.js
+++ handlers/home.js
@@ -1,5 +1,6 @@
 const url = require('url');
 const fs = require('fs');
+const qs = require('querystring');
 const path = require('path');
 const cats = require('../data/cats');
 const breeds = require('../data/breeds');
@@ -21,6 +22,35 @@ module.exports = (req, res) => {
             }
 
             const modifiedCats = cats.map((cat) => `
+            <li>
+            <img src="${path.join('./content/images/' + cat.image)}" alt="${cat.name}">
+            <h3>${cat.name}</h3>
+            <p><span>Breed: </span>${cat.breed}</p>
+            <p><span>Description: </span>${cat.description}</p>
+            <ul class="buttons">
+                <li class="btn edit"><a href="/cats-edit/${cat.id}">Change Info</a></li>
+                <li class="btn delete"><a href="/cats-find-new-home/${cat.id}">New Home</a></li>
+            </ul>
+            </li>`)
+
+            const modifiedData = data.toString().replace('{{cats}}', modifiedCats)
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(modifiedData);
+            res.end();
+        });
+    } else if (pathname.includes('/search') && req.method === "GET") {
+        const searchQuery = qs.parse(req.url)['/search?search'].toLowerCase();
+        const catsFound = cats.filter(cat => cat.name.toLowerCase().includes(searchQuery));
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                console.log(err);
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.write('404 File Not Found');
+                res.end();
+                return;
+            }
+
+            const modifiedCats = catsFound.map((cat) => `
             <li>
             <img src="${path.join('./content/images/' + cat.image)}" alt="${cat.name}">
             <h3>${cat.name}</h3>
